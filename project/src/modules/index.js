2a6631f11afc6e2c484c811be8fbc2df74b5d144
Chaodi Gao <759132976@qq.com>
2017-05-07 12:01:23 +0800

    书架及书籍详情

--- project/src/modules/index.js
+++ project/src/modules/index.js
@@ -1,9 +1,11 @@
 import Main from './Main'
 import Login from './Login'
 import Read from './Read'
+import Detail from './Detail'
 
 export default {
   Main,
   Login,
-  Read
+  Read,
+  Detail
 }
